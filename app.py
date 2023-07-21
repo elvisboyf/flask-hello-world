e51f02cdb3de52b443a73ea134f7dffff7924404
elvisboyf <77184979+elvisboyf@users.noreply.github.com>
2023-07-21 01:09:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def receive_trading_signal():
         
         if posicion == "buy":
             posicion="nulo"
-            precioC = float(orders[1]["entryPrice"])-(float(orders[1]["entryPrice"])*0.015)
+            precioC = float(orders[1]["entryPrice"])-(float(orders[1]["entryPrice"])*0.01)
             print("Precio para comprar: "+str(precioC))
             if abs(float(orders[2]["positionAmt"])) != 0 and float(orders[2]["unRealizedProfit"]) >= 0:
                 print("CERRAREMOS VENTA")  
@@ -104,7 +104,7 @@ def receive_trading_signal():
 
         elif posicion == "sell":
             posicion="nulo"
-            precioV = float(orders[2]["entryPrice"])+(float(orders[2]["entryPrice"])*0.015)
+            precioV = float(orders[2]["entryPrice"])+(float(orders[2]["entryPrice"])*0.01)
             print("Precio para vender: "+str(precioV))
             if abs(float(orders[1]["positionAmt"])) != 0 and float(orders[1]["unRealizedProfit"]) >= 0:
                 print("CERRAREMOS COMPRA") 
